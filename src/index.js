d26166400971b964e810c2bfa9576fb196617026
Jose Nicdao <jonicdao@gmail.com>
2020-05-01 17:00:23 -0700

    react router

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'font-awesome/css/font-awesome.css';
-import 'bootstrap-social/bootstrap-social.css';
+// import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
